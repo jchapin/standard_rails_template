ceac94ef85e066ef0a55053883840f555b11052e
John Chapin <john@cts-llc.net>
2020-05-15 16:29:50 -0400

    Not necessary.

--- template.rb
+++ template.rb
@@ -286,8 +286,6 @@ gem_group :development, :test do
   # Remove SQLite from ALL environments. Only want it in development and test.
   gsub_file 'Gemfile', /^# Use sqlite3 as the database for Active Record\n/, ''
   gsub_file 'Gemfile', /^gem 'sqlite3'\n/, ''
-  # Add SQLite to development and test.
-  gem 'sqlite3', '< 1.4.0'
 end
 
 # Rubocop Configuration File
